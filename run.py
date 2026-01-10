c4189d04c76a4d2c84cb26c71622800efb456c5a
aprillins <aprillins@gmail.com>
2026-01-10 20:40:21 +0700

    add run.py for running server using fast api

---
+++ run.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+import gradio as gr
+from main import demo
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+app = gr.mount_gradio_app(app, demo, path="/gradio")

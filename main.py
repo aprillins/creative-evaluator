55f142edc5ea634a5206bed07a7306149cb6eb02
aprillins <aprillins@gmail.com>
2026-01-10 09:49:20 +0700

    add structured output

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import os
-from typing import Dict
+from typing import Dict, Literal
 from dotenv import load_dotenv
 import gradio as gr
+from langchain.agents import create_agent
 import langchain_core
 import langchain_community
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_openai import ChatOpenAI
 import base64
 from io import BytesIO
 from PIL import Image
+from pydantic import BaseModel, Field
 
 load_dotenv()
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 llm = ChatOpenAI(model="gpt-4.1-mini")
+
+
+class ImageReadingResult(BaseModel):
+    description: str = Field(..., description="Detailed description of the image content.")
+    type_of_promotion: Literal["Acquisition", "Retention", "Others", "Unclear"] = Field(..., description="Type of promotion depicted in the image.")
+    promo_font_size_readability: Literal["Small", "Medium", "Large", "Unclear"] = Field(..., description="Assessment of promo font size readability compared to the whole image.")
+
 
 def get_basic_image_info(img: Image.Image):
     img_context = (
@@ -45,13 +54,28 @@ def evaluate_creativity(image: Image.Image) -> str:
     if image is not None:
         img = pil_image_to_base64(image)
         img_b64 = img['base64']
-        img_info = img['info']
+        img_info = img['info']  
+        message = SystemMessage(
+            content = """
+            You are a ad creative evaluator. You give a detailed assessment of ad creative readability.
+            Imagine you are a user seeing this image for the first time. You should ask yourself these questions:
+            1. Is the message clear?
+            2. Is the font size appropriate?
+            3. Are the text colors contrasting well with the background?
+            4. Is the safe zone already applied?
+            5. Do you understand the meaning of the image?
+            6. What type of promotion is this? (Acquisition, Retention, Others, Unclear)
+            """)
+        messages = [message]
+
         message = HumanMessage(
             content = [
                 {"type": "text", "text": f"What is in this image? Describe it in detail. Additionally, it has the following properties:\n{img_info}"},
                 {"type": "image", "base64": img_b64, "mime_type": "image/jpeg"}
             ]
         )
+        messages.append(message)
+
 
         ## alternative method to use url
         # img_data_uri = f"data:image/jpeg;base64,{img_b64}"
@@ -63,12 +87,14 @@ def evaluate_creativity(image: Image.Image) -> str:
         # )
 
         try:
-            response = llm.invoke([message])
+            # response = llm.invoke(messages)
+            llm_with_structured_response = llm.with_structured_output(ImageReadingResult)
+            structured_response = llm_with_structured_response.invoke(messages)
         except Exception as e:
             print(f"Error invoking LLM: {e}")
             return f"Error processing image: {e}"
 
-        return response.content
+        return structured_response
     else:
         return "No image uploaded."
 
